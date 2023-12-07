90c36e0438184989db4171d498be5a86bdc8782b
Darian T. Yang <dty7@pitt.edu>
2023-12-07 13:25:53 -0500

    ran stat analysis for GDC inter contact features

---
+++ gdc/WT_GDC_inter_stat_analysis/jensen_shannon_Pymol_Per_Feature_Scores.py
@@ -0,0 +1,135 @@
+# You can run me in several ways, perhaps the easiest way is to:
+# 1. Load the PDB file of your system in PyMOL.
+# 2. Type: @[FILE_NAME.py] in the command line.
+# Make sure the .py files are in the same directory as the pdb.
+# The lines below are suggestions for potentially nicer figures.
+# You can comment them in if you want.
+# bg_color white
+# set cartoon_color, grey90
+# set ray_opaque_background, 0
+# set antialias, 2
+# set ray_shadows, 0
+distance interaction0, resid 54 and name CA, resid 142 and name CA 
+set dash_radius, 0.5, interaction0 
+set dash_color, red, interaction0 
+distance interaction1, resid 40 and name CA, resid 172 and name CA 
+set dash_radius, 0.4934, interaction1 
+set dash_color, red, interaction1 
+distance interaction2, resid 45 and name CA, resid 146 and name CA 
+set dash_radius, 0.4927, interaction2 
+set dash_color, magenta, interaction2 
+distance interaction3, resid 54 and name CA, resid 156 and name CA 
+set dash_radius, 0.4912, interaction3 
+set dash_color, red, interaction3 
+distance interaction4, resid 56 and name CA, resid 142 and name CA 
+set dash_radius, 0.4909, interaction4 
+set dash_color, red, interaction4 
+distance interaction5, resid 41 and name CA, resid 172 and name CA 
+set dash_radius, 0.4902, interaction5 
+set dash_color, magenta, interaction5 
+distance interaction6, resid 54 and name CA, resid 144 and name CA 
+set dash_radius, 0.4886, interaction6 
+set dash_color, red, interaction6 
+distance interaction7, resid 81 and name CA, resid 144 and name CA 
+set dash_radius, 0.487, interaction7 
+set dash_color, green, interaction7 
+distance interaction8, resid 83 and name CA, resid 171 and name CA 
+set dash_radius, 0.4803, interaction8 
+set dash_color, red, interaction8 
+distance interaction9, resid 43 and name CA, resid 146 and name CA 
+set dash_radius, 0.4798, interaction9 
+set dash_color, magenta, interaction9 
+distance interaction10, resid 55 and name CA, resid 142 and name CA 
+set dash_radius, 0.4768, interaction10 
+set dash_color, magenta, interaction10 
+distance interaction11, resid 52 and name CA, resid 157 and name CA 
+set dash_radius, 0.4762, interaction11 
+set dash_color, magenta, interaction11 
+distance interaction12, resid 52 and name CA, resid 156 and name CA 
+set dash_radius, 0.4677, interaction12 
+set dash_color, magenta, interaction12 
+distance interaction13, resid 53 and name CA, resid 157 and name CA 
+set dash_radius, 0.4663, interaction13 
+set dash_color, magenta, interaction13 
+distance interaction14, resid 56 and name CA, resid 144 and name CA 
+set dash_radius, 0.4606, interaction14 
+set dash_color, green, interaction14 
+distance interaction15, resid 81 and name CA, resid 169 and name CA 
+set dash_radius, 0.4538, interaction15 
+set dash_color, green, interaction15 
+distance interaction16, resid 59 and name CA, resid 171 and name CA 
+set dash_radius, 0.4484, interaction16 
+set dash_color, blue, interaction16 
+distance interaction17, resid 51 and name CA, resid 155 and name CA 
+set dash_radius, 0.4457, interaction17 
+set dash_color, red, interaction17 
+distance interaction18, resid 52 and name CA, resid 155 and name CA 
+set dash_radius, 0.4357, interaction18 
+set dash_color, red, interaction18 
+distance interaction19, resid 79 and name CA, resid 147 and name CA 
+set dash_radius, 0.4286, interaction19 
+set dash_color, red, interaction19 
+distance interaction20, resid 80 and name CA, resid 146 and name CA 
+set dash_radius, 0.4201, interaction20 
+set dash_color, magenta, interaction20 
+distance interaction21, resid 45 and name CA, resid 144 and name CA 
+set dash_radius, 0.4029, interaction21 
+set dash_color, magenta, interaction21 
+distance interaction22, resid 82 and name CA, resid 171 and name CA 
+set dash_radius, 0.389, interaction22 
+set dash_color, magenta, interaction22 
+distance interaction23, resid 81 and name CA, resid 146 and name CA 
+set dash_radius, 0.378, interaction23 
+set dash_color, magenta, interaction23 
+distance interaction24, resid 40 and name CA, resid 171 and name CA 
+set dash_radius, 0.3772, interaction24 
+set dash_color, red, interaction24 
+distance interaction25, resid 56 and name CA, resid 167 and name CA 
+set dash_radius, 0.331, interaction25 
+set dash_color, magenta, interaction25 
+distance interaction26, resid 56 and name CA, resid 170 and name CA 
+set dash_radius, 0.3291, interaction26 
+set dash_color, green, interaction26 
+distance interaction27, resid 79 and name CA, resid 146 and name CA 
+set dash_radius, 0.3218, interaction27 
+set dash_color, magenta, interaction27 
+distance interaction28, resid 53 and name CA, resid 141 and name CA 
+set dash_radius, 0.3205, interaction28 
+set dash_color, magenta, interaction28 
+distance interaction29, resid 58 and name CA, resid 172 and name CA 
+set dash_radius, 0.2902, interaction29 
+set dash_color, red, interaction29 
+distance interaction30, resid 81 and name CA, resid 129 and name CA 
+set dash_radius, 0.2854, interaction30 
+set dash_color, magenta, interaction30 
+distance interaction31, resid 41 and name CA, resid 171 and name CA 
+set dash_radius, 0.2829, interaction31 
+set dash_color, magenta, interaction31 
+distance interaction32, resid 40 and name CA, resid 173 and name CA 
+set dash_radius, 0.2539, interaction32 
+set dash_color, red, interaction32 
+distance interaction33, resid 82 and name CA, resid 146 and name CA 
+set dash_radius, 0.2307, interaction33 
+set dash_color, magenta, interaction33 
+distance interaction34, resid 59 and name CA, resid 173 and name CA 
+set dash_radius, 0.2177, interaction34 
+set dash_color, red, interaction34 
+distance interaction35, resid 56 and name CA, resid 169 and name CA 
+set dash_radius, 0.2002, interaction35 
+set dash_color, green, interaction35 
+distance interaction36, resid 54 and name CA, resid 143 and name CA 
+set dash_radius, 0.1941, interaction36 
+set dash_color, magenta, interaction36 
+distance interaction37, resid 59 and name CA, resid 172 and name CA 
+set dash_radius, 0.1293, interaction37 
+set dash_color, red, interaction37 
+distance interaction38, resid 43 and name CA, resid 144 and name CA 
+set dash_radius, 0.112, interaction38 
+set dash_color, magenta, interaction38 
+distance interaction39, resid 83 and name CA, resid 173 and name CA 
+set dash_radius, 0.0592, interaction39 
+set dash_color, red, interaction39 
+group All_Interactions, interaction* 
+set dash_gap, 0.00, All_Interactions 
+set dash_round_ends, off, All_Interactions 
+hide labels 
---
+++ gdc/WT_GDC_inter_stat_analysis/mutual_information_Pymol_Per_Feature_Scores.py
@@ -0,0 +1,135 @@
+# You can run me in several ways, perhaps the easiest way is to:
+# 1. Load the PDB file of your system in PyMOL.
+# 2. Type: @[FILE_NAME.py] in the command line.
+# Make sure the .py files are in the same directory as the pdb.
+# The lines below are suggestions for potentially nicer figures.
+# You can comment them in if you want.
+# bg_color white
+# set cartoon_color, grey90
+# set ray_opaque_background, 0
+# set antialias, 2
+# set ray_shadows, 0
+distance interaction0, resid 54 and name CA, resid 156 and name CA 
+set dash_radius, 0.5, interaction0 
+set dash_color, red, interaction0 
+distance interaction1, resid 54 and name CA, resid 144 and name CA 
+set dash_radius, 0.4816, interaction1 
+set dash_color, red, interaction1 
+distance interaction2, resid 52 and name CA, resid 155 and name CA 
+set dash_radius, 0.4578, interaction2 
+set dash_color, red, interaction2 
+distance interaction3, resid 51 and name CA, resid 155 and name CA 
+set dash_radius, 0.4539, interaction3 
+set dash_color, red, interaction3 
+distance interaction4, resid 52 and name CA, resid 156 and name CA 
+set dash_radius, 0.4505, interaction4 
+set dash_color, magenta, interaction4 
+distance interaction5, resid 81 and name CA, resid 144 and name CA 
+set dash_radius, 0.4418, interaction5 
+set dash_color, green, interaction5 
+distance interaction6, resid 54 and name CA, resid 142 and name CA 
+set dash_radius, 0.4388, interaction6 
+set dash_color, red, interaction6 
+distance interaction7, resid 40 and name CA, resid 172 and name CA 
+set dash_radius, 0.4285, interaction7 
+set dash_color, red, interaction7 
+distance interaction8, resid 45 and name CA, resid 146 and name CA 
+set dash_radius, 0.427, interaction8 
+set dash_color, magenta, interaction8 
+distance interaction9, resid 79 and name CA, resid 147 and name CA 
+set dash_radius, 0.4217, interaction9 
+set dash_color, red, interaction9 
+distance interaction10, resid 41 and name CA, resid 172 and name CA 
+set dash_radius, 0.4217, interaction10 
+set dash_color, magenta, interaction10 
+distance interaction11, resid 52 and name CA, resid 157 and name CA 
+set dash_radius, 0.4121, interaction11 
+set dash_color, magenta, interaction11 
+distance interaction12, resid 55 and name CA, resid 142 and name CA 
+set dash_radius, 0.407, interaction12 
+set dash_color, magenta, interaction12 
+distance interaction13, resid 53 and name CA, resid 157 and name CA 
+set dash_radius, 0.4065, interaction13 
+set dash_color, magenta, interaction13 
+distance interaction14, resid 83 and name CA, resid 171 and name CA 
+set dash_radius, 0.3959, interaction14 
+set dash_color, red, interaction14 
+distance interaction15, resid 59 and name CA, resid 171 and name CA 
+set dash_radius, 0.3891, interaction15 
+set dash_color, blue, interaction15 
+distance interaction16, resid 56 and name CA, resid 142 and name CA 
+set dash_radius, 0.3868, interaction16 
+set dash_color, red, interaction16 
+distance interaction17, resid 40 and name CA, resid 171 and name CA 
+set dash_radius, 0.3722, interaction17 
+set dash_color, red, interaction17 
+distance interaction18, resid 81 and name CA, resid 169 and name CA 
+set dash_radius, 0.3647, interaction18 
+set dash_color, green, interaction18 
+distance interaction19, resid 43 and name CA, resid 146 and name CA 
+set dash_radius, 0.3623, interaction19 
+set dash_color, magenta, interaction19 
+distance interaction20, resid 82 and name CA, resid 171 and name CA 
+set dash_radius, 0.3601, interaction20 
+set dash_color, magenta, interaction20 
+distance interaction21, resid 45 and name CA, resid 144 and name CA 
+set dash_radius, 0.3505, interaction21 
+set dash_color, magenta, interaction21 
+distance interaction22, resid 56 and name CA, resid 144 and name CA 
+set dash_radius, 0.3464, interaction22 
+set dash_color, green, interaction22 
+distance interaction23, resid 56 and name CA, resid 167 and name CA 
+set dash_radius, 0.3011, interaction23 
+set dash_color, magenta, interaction23 
+distance interaction24, resid 80 and name CA, resid 146 and name CA 
+set dash_radius, 0.2832, interaction24 
+set dash_color, magenta, interaction24 
+distance interaction25, resid 40 and name CA, resid 173 and name CA 
+set dash_radius, 0.2365, interaction25 
+set dash_color, red, interaction25 
+distance interaction26, resid 53 and name CA, resid 141 and name CA 
+set dash_radius, 0.2288, interaction26 
+set dash_color, magenta, interaction26 
+distance interaction27, resid 81 and name CA, resid 146 and name CA 
+set dash_radius, 0.2074, interaction27 
+set dash_color, magenta, interaction27 
+distance interaction28, resid 56 and name CA, resid 170 and name CA 
+set dash_radius, 0.1832, interaction28 
+set dash_color, green, interaction28 
+distance interaction29, resid 54 and name CA, resid 143 and name CA 
+set dash_radius, 0.1698, interaction29 
+set dash_color, magenta, interaction29 
+distance interaction30, resid 79 and name CA, resid 146 and name CA 
+set dash_radius, 0.1632, interaction30 
+set dash_color, magenta, interaction30 
+distance interaction31, resid 58 and name CA, resid 172 and name CA 
+set dash_radius, 0.1402, interaction31 
+set dash_color, red, interaction31 
+distance interaction32, resid 81 and name CA, resid 129 and name CA 
+set dash_radius, 0.1106, interaction32 
+set dash_color, magenta, interaction32 
+distance interaction33, resid 41 and name CA, resid 171 and name CA 
+set dash_radius, 0.1015, interaction33 
+set dash_color, magenta, interaction33 
+distance interaction34, resid 56 and name CA, resid 169 and name CA 
+set dash_radius, 0.0934, interaction34 
+set dash_color, green, interaction34 
+distance interaction35, resid 59 and name CA, resid 173 and name CA 
+set dash_radius, 0.0744, interaction35 
+set dash_color, red, interaction35 
+distance interaction36, resid 43 and name CA, resid 144 and name CA 
+set dash_radius, 0.0743, interaction36 
+set dash_color, magenta, interaction36 
+distance interaction37, resid 82 and name CA, resid 146 and name CA 
+set dash_radius, 0.0732, interaction37 
+set dash_color, magenta, interaction37 
+distance interaction38, resid 83 and name CA, resid 173 and name CA 
+set dash_radius, 0.0618, interaction38 
+set dash_color, red, interaction38 
+distance interaction39, resid 59 and name CA, resid 172 and name CA 
+set dash_radius, 0.0453, interaction39 
+set dash_color, red, interaction39 
+group All_Interactions, interaction* 
+set dash_gap, 0.00, All_Interactions 
+set dash_round_ends, off, All_Interactions 
+hide labels 
